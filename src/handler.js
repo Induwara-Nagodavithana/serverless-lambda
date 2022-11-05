1670fb5be4f9dee000dcedce61292a70ee02b879
Induwara-Nagodavithana <induwara.nagodavithana@gmail.com>
2022-11-06 01:00:45 +0530

    testing

--- src/handler.js
+++ src/handler.js
@@ -31,33 +31,38 @@ module.exports.createUser = async (event) => {
   console.log("sadfasdf user");
 
   user
-        .save()
-        .then((user) => {
-          return {
-            statusCode: 200,
-            body: JSON.stringify(
-              {
-                message: user,
-              },
-              null,
-              2
-            ),
-          };
-        })
-        .catch((err) => {
-          return {
-            statusCode: 400,
-            body: JSON.stringify(
-              {
-                message: "User cannot created!",
-                error: err,
-              },
-              null,
-              2
-            ),
-          };
-        });
-
+    .save()
+    .then((user) => {
+      console.log(user);
+      console.log("Done user");
+      return {
+        statusCode: 200,
+        body: JSON.stringify(
+          {
+            message: user,
+          },
+          null,
+          2
+        ),
+      };
+    })
+    .catch((err) => {
+      console.log(err);
+      console.log("Error user");
+      return {
+        statusCode: 400,
+        body: JSON.stringify(
+          {
+            message: "User cannot created!",
+            error: err,
+          },
+          null,
+          2
+        ),
+      };
+    });
+    // console.log(err);
+    console.log("All Done user");
   // bcrypt.genSalt(saltRounds, function (err, salt) {
   //   bcrypt.hash(event.password, salt, function (err, hash) {
   //     event.password = hash;
